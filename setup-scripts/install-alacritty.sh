415a5e1a8ee05a95d92bb1190b83c828c4235d57
Kai Guo <kguo@fortinet.com>
2022-08-06 22:05:05 -0700

    Update alacrity script

--- setup-scripts/install-alacritty.sh
+++ setup-scripts/install-alacritty.sh
@@ -8,7 +8,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup override set stable
 rustup update stable
 
-sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+sudo apt-get install -yy cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 cargo build --release
 
