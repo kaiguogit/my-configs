486173333f73c0503865542a5190c56917b8f3ce
Kai Guo <kguo@fortinet.com>
2022-08-07 11:11:20 -0700

    Export screen saver setting

--- gnome/export.sh
+++ gnome/export.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
 dconf dump /com/github/ > $SCRIPT_DIR/com-github.conf
 dconf dump /org/gnome/shell/ > $SCRIPT_DIR/org-gnome-shell.conf
+dconf dump /org/gnome/desktop/screensaver/ > $SCRIPT_DIR/org-gnome-desktop-screensaver.conf
--- gnome/import.sh
+++ gnome/import.sh
@@ -3,3 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 dconf load /com/github/ < $SCRIPT_DIR/com-github.conf
 dconf load /org/gnome/shell/ < $SCRIPT_DIR/org-gnome-shell.conf
+dconf load /org/gnome/desktop/screensaver/ < $SCRIPT_DIR/org-gnome-desktop-screensaver.conf
