7dc0bd4fa8d20de7873ac887a05049f6f2fc6d18
Kai Guo <kguo@fortinet.com>
2022-10-05 09:45:34 -0700

    update zsh script and alacrity default shell

--- setup-scripts/automated/zsh.sh
+++ setup-scripts/automated/zsh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
+
 # https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH
 sudo apt-get install -yy zsh
 chsh -s $(which zsh)
@@ -8,8 +9,9 @@ chsh -s $(which zsh)
 # install zoxide
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
