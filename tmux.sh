32b90dccd96de8911c7c6955c3be30412d345919
Kai Guo <kguo@fortinet.com>
2022-11-09 10:52:42 -0800

    Clear timeout in tmux

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # https://www.reddit.com/r/tmux/comments/jj35jf/launching_tmux_in_a_predefined_layout_from_the/
+tmux setenv -ug TMOUT
 tmux new-session -d
 tmux split-window -h
 tmux split-window -v
 tmux select-pane -t 0
 tmux attach-session -d
-
 
 
