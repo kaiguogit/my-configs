a6d92ea98e4d42f182f8051c3f1385e6c4f277ca
Kai Guo <kguo@fortinet.com>
2024-01-02 09:42:38 -0800

    save individual monitor scale settings

--- gnome/import.sh
+++ gnome/import.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 dconf load /com/github/ < $SCRIPT_DIR/com-github.conf
 dconf load /org/gnome/shell/ < $SCRIPT_DIR/org-gnome-shell.conf
+dconf load /org/gnome/mutter/ < $SCRIPT_DIR/org-gnome-mutter.conf
 dconf load /org/gnome/desktop/screensaver/ < $SCRIPT_DIR/org-gnome-desktop-screensaver.conf
 dconf load /org/gnome/desktop/notifications/application/ < $SCRIPT_DIR/org-gnome-desktop-notifications-application.conf
 dconf load /org/gnome/desktop/interface/ < $SCRIPT_DIR/org-gnome-desktop-interface.conf
