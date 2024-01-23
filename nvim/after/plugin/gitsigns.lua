86bb90258320b343ef64d6810baa119ea8c431cd
Kai Guo <kguo@fortinet.com>
2024-01-23 11:40:13 -0800

    Update gitsign

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,4 +1,7 @@
 require('gitsigns').setup{
+    current_line_blame = true,
+    signcolumn = true,
+    auto_attach= true,
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
