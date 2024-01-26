c0fc82bfc5a2f71132b851f92dd0df5d71eb65c5
Kai Guo <kguo@fortinet.com>
2024-01-26 09:17:22 -0800

    Update tmux.sh initial script

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # https://www.reddit.com/r/tmux/comments/jj35jf/launching_tmux_in_a_predefined_layout_from_the/
 tmux setenv -ug TMOUT
-tmux new-session -d
-tmux new-window
-tmux split-window -h
-tmux split-window -v
-tmux select-pane -t 0
-tmux select-window -p
-tmux attach-session -d
+
+start_session() {
+    tmux new-session -d -s "$1" -c "$2" "/usr/bin/nvim ."
+    tmux new-window -c "$2"
+    tmux split-window -h -c "$2$3"
+    tmux split-window -v -c "$2$4"
+    tmux select-pane -t 0
+    tmux select-window -p
+}
+
+start_session fos ~/build/fos-ci/fortios-ci /migadmin/pkg/angular /migadmin/pkg/angular
+start_session neutrino ~/build/neutrino-gitlab/neutrino
+start_session login_page ~/build/fos-ci/worktree/3 /migadmin/pkg/angular /migadmin/pkg/angular
+start_session lint ~/build/fos-ci/worktree/1 /migadmin/pkg/angular /migadmin/pkg/angular
+
+
+tmux attach-session -d -t fos
+
