87bceb3d57086d5c8992cb14303826b6422a55f6
Kai Guo <kguo@fortinet.com>
2024-06-04 10:43:20 -0700

    Update git log lines

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,8 +1,18 @@
 local silentopts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>gb", "<cmd>G blame<CR>")
-vim.keymap.set("n", "<leader>gfh", ":G log --follow -p -20 -- %<CR>", silentopts)
+vim.keymap.set("n", "<leader>gfh", ":G log --pretty=oneline --follow -p -20 -- %<CR>", silentopts)
 
-vim.keymap.set('v', '<leader>gl', function()
-    vim.print("G log -L" .. vim.fn.line('v') .. "," .. vim.fn.line('.') .. ":" .. vim.fn.expand('%:p'))
-    vim.cmd("G log -L" .. vim.fn.line('v') .. "," .. vim.fn.line('.') .. ":" .. vim.fn.expand('%:p'))
+vim.keymap.set("v", "<leader>gl", function()
+	vim.cmd("G log --pretty=oneline -L" .. vim.fn.line("v") .. "," .. vim.fn.line(".") .. ":" .. vim.fn.expand("%:p"))
+end, {})
+
+vim.keymap.set("n", "<leader>gl", function()
+	vim.cmd(
+		"G log --pretty=oneline -L"
+			.. math.max(1, vim.fn.line(".") - 1)
+			.. ","
+			.. vim.fn.line(".") + 1
+			.. ":"
+			.. vim.fn.expand("%:p")
+	)
 end, {})
