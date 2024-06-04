ea67cca8e0d1691379d51defc8f7e00f7806de40
Kai Guo <kguo@fortinet.com>
2024-06-04 00:26:46 -0700

    Add fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,8 @@
 local silentopts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>gb", "<cmd>G blame<CR>")
 vim.keymap.set("n", "<leader>gfh", ":G log --follow -p -20 -- %<CR>", silentopts)
+
+vim.keymap.set('v', '<leader>gl', function()
+    vim.print("G log -L" .. vim.fn.line('v') .. "," .. vim.fn.line('.') .. ":" .. vim.fn.expand('%:p'))
+    vim.cmd("G log -L" .. vim.fn.line('v') .. "," .. vim.fn.line('.') .. ":" .. vim.fn.expand('%:p'))
+end, {})
