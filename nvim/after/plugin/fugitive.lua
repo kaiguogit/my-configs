0e13111369579efab1734507c0d27e5442778a2a
Kai Guo <kguo@fortinet.com>
2024-06-07 10:19:44 -0700

    update gl

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -3,12 +3,19 @@ vim.keymap.set("n", "<leader>gb", "<cmd>G blame<CR>")
 vim.keymap.set("n", "<leader>gfh", ":G log --pretty=oneline --follow -p -20 -- %<CR>", silentopts)
 
 vim.keymap.set("v", "<leader>gl", function()
-	vim.cmd("G log --pretty=oneline -L" .. vim.fn.line("v") .. "," .. vim.fn.line(".") .. ":" .. vim.fn.expand("%:p"))
+	vim.cmd(
+		'G log --pretty="tformat: %Creset%n****************************************************************************************%n%Cred%h%Creset %Cgreen%s%Creset" -L'
+			.. vim.fn.line("v")
+			.. ","
+			.. vim.fn.line(".")
+			.. ":"
+			.. vim.fn.expand("%:p")
+	)
 end, {})
 
 vim.keymap.set("n", "<leader>gl", function()
 	vim.cmd(
-		"G log --pretty=oneline -L"
+		'G log --pretty="tformat: %Creset%n****************************************************************************************%n%Cred%h%Creset %Cgreen%s%Creset" -L'
 			.. math.max(1, vim.fn.line(".") - 1)
 			.. ","
 			.. vim.fn.line(".") + 1
