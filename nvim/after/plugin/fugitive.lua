ff5a1c6d19bee478d2e2b0f2d8a73c868e92f4a0
Kai Guo <kguo@fortinet.com>
2024-07-02 23:59:32 -0700

    Use vertical fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,11 +1,13 @@
 local silentopts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>gb", "<cmd>G blame<CR>")
-vim.keymap.set("n", "<leader>gfh", ":G log --pretty=oneline --follow -p -20 -- %<CR>", silentopts)
-vim.keymap.set("n", "<leader>ggl", ":G log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit<CR>", silentopts)
+vim.keymap.set("n", "<leader>gsh", ":vert G --paginate show <C-r>+ -- %<Left><Left><Left><Left><Left>", silentopts)
+vim.keymap.set("n", "<leader>gfh", ":vert G --paginate log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --follow -p -20 -- %<CR>", silentopts)
+vim.keymap.set("n", "<leader>ggl", ":vert G --paginate log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit<CR>", silentopts)
 
 vim.keymap.set("v", "<leader>gl", function()
 	vim.cmd(
-		'G log --pretty="tformat: %Creset%n****************************************************************************************%n%Cred%h%Creset %Cgreen%s%Creset (%cr)" -L'
+		'vert G --paginate log --pretty="tformat: %Creset%n****************************************************************************************'
+            .. '%n%Cred%h%Creset %Cgreen%s%Creset%Cgreen(%cr) %C(bold blue)<%an>%Creset" -L'
 			.. vim.fn.line("v")
 			.. ","
 			.. vim.fn.line(".")
@@ -16,7 +18,8 @@ end, {})
 
 vim.keymap.set("n", "<leader>gl", function()
 	vim.cmd(
-		'G log --pretty="tformat: %Creset%n****************************************************************************************%n%Cred%h%Creset %Cgreen%s%Creset (%cr)" -L'
+		'vert G --paginate log --pretty="tformat: %Creset%n****************************************************************************************'
+            .. '%n%Cred%h%Creset %Cgreen%s%Creset%Cgreen(%cr) %C(bold blue)<%an>%Creset" -L'
 			.. math.max(1, vim.fn.line(".") - 1)
 			.. ","
 			.. vim.fn.line(".") + 1
