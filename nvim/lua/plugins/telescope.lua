daee9538449cb93a3cafe401e3f97c9f35c7a2d2
Kai Guo <kguo@fortinet.com>
2025-04-18 16:24:32 -0700

    Fix neoclip

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,6 +5,33 @@ return {
 			{ "kkharji/sqlite.lua" },
 			{ "nvim-telescope/telescope.nvim" },
 		},
+		config = function()
+			local function is_whitespace(line)
+				return vim.fn.match(line, [[^\s*$]]) ~= -1
+			end
+
+			local function all(tbl, check)
+				for _, entry in ipairs(tbl) do
+					if not check(entry) then
+						return false
+					end
+				end
+				return true
+			end
+
+			require("neoclip").setup({
+				keys = {
+					telescope = {
+						i = {
+							paste = "<cr>",
+						},
+					},
+				},
+				filter = function(data)
+					return not all(data.event.regcontents, is_whitespace)
+				end,
+			})
+		end,
 	},
 	{
 		"nvim-telescope/telescope.nvim",
