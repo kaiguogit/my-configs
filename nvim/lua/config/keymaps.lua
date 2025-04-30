099c5f801e86af76decc8f19ee5d3fc309196dea
Kai Guo <kguo@fortinet.com>
2025-04-29 23:32:55 -0700

    Remap q

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -340,3 +340,14 @@ vim.keymap.set("n", "<A-n>", function ()
 	open_next_file({reverse = true})
 end)
 vim.keymap.set("n", "<A-m>", open_next_file)
+
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+
+vim.keymap.set('n', 'Q', 'q', { noremap = true, silent = true })
+vim.keymap.set('n', 'q', '<Nop>', { noremap = true, silent = true })
+
