91e17912be7bc6bf775d87982d7078d5ca5cc63d
Kai Guo <kguo@fortinet.com>
2025-05-12 17:18:46 -0700

    Backup kvm

---
+++ backup_kvm.sh
@@ -0,0 +1,3 @@
+sudo cp -r /etc/libvirt/qemu/* ~/build/my-configs/kvm/client/
+sudo cp /etc/ufw/before.rules ~/build/my-configs/ufw
+sudo cp /etc/libvirt/hooks/qemu ~/build/my-configs/kvm/hooks
---
+++ sync_kvm.sh
@@ -0,0 +1,4 @@
+sudo cp -r ~/build/my-configs/kvm/client/* /etc/libvirt/qemu/
+sudo cp ~/build/my-configs/ufw/before.rules /etc/ufw/before.rules
+sudo cp ~/build/my-configs/kvm/hooks/qemu /etc/libvirt/hooks/
+
