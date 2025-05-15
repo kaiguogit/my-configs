40e9fb670e61b86e6dd493ca417f416d91b4072d
Kai Guo <kguo@fortinet.com>
2025-05-14 19:41:02 -0700

    Update syncthings

---
+++ setup-scripts/automated/syncthings.sh
@@ -0,0 +1,8 @@
+# Add the release PGP keys:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install -yy syncthing 
+sudo ufw allow syncthing
