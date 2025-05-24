e364e73d85fd7f1e82a48177c75c9e616fb711ef
Kai Guo <kguo@fortinet.com>
2025-05-24 10:03:15 -0700

    Use gD for lsp.definitions

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -134,13 +134,13 @@ return {
 			-- { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
 			--lsp
 			-- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-			{
-				"gD",
-				function()
-					Snacks.picker.lsp_declarations()
-				end,
-				desc = "Goto Declaration",
-			},
+			-- {
+			-- 	"gD",
+			-- 	function()
+			-- 		Snacks.picker.lsp_declarations()
+			-- 	end,
+			-- 	desc = "Goto Declaration",
+			-- },
 			{
 				"gr",
 				function()
