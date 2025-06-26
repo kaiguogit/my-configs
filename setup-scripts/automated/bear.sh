8add7b3e3650bf496055505e725f4b4f9079a59c
Kai Guo <kguo@fortinet.com>
2025-06-25 17:59:04 -0700

    Update bear

--- setup-scripts/automated/bear.sh
+++ setup-scripts/automated/bear.sh
@@ -9,6 +9,6 @@ cp $SCRIPT_DIR/../../bear ~/bin/
 mkdir -p ~/build/open-source/bear-tools
 
 # bear 2.4.4-1 is a bear binary built from 2.4.4 using fortidev-6. Michael Thomas gave me it.
-tar xvf $SCRIPT_DIR/../../bear-2.4.4-1.tar.xz -C ~/open-source/bear-tools
+tar xvf $SCRIPT_DIR/../../bear-2.4.4-1.tar.xz -C ~/build/open-source/bear-tools
 
 
