03d2cfb341dc0f9db4159342ee2a848d3c8351ad
Kai Guo <kguo@fortinet.com>
2025-07-06 10:50:30 -0700

    Remove marks

--- nvim/lua/plugins/marks.lua
+++ nvim/lua/plugins/marks.lua
@@ -1,32 +1,31 @@
 return {
-  {
-    "chentoast/marks.nvim",
-    opts = {
-      refresh_interval = 500,
-    },
-    config = function(_, opts)
-      require("marks").setup(opts)
-
-      require("which-key").add({
-        { "m", group = "Marks", desc = "Set mark" },
-        { "m]", desc = "Next mark" },
-        { "m[", desc = "Previous mark" },
-        { "m}", desc = "Next bookmark" },
-        { "m{", desc = "Previous bookmark" },
-        { "m,", desc = "Set next mark" },
-        { "m;", desc = "Toggle mark" },
-        { "m:", desc = "Preview mark" },
-        { "m0", desc = "Set bookmark 0" },
-        { "m1", desc = "Set bookmark 1" },
-        { "m2", desc = "Set bookmark 2" },
-        { "m3", desc = "Set bookmark 3" },
-        { "m4", desc = "Set bookmark 4" },
-        { "m5", desc = "Set bookmark 5" },
-        { "m6", desc = "Set bookmark 6" },
-        { "m7", desc = "Set bookmark 7" },
-        { "m8", desc = "Set bookmark 8" },
-        { "m9", desc = "Set bookmark 9" },
-      })
-    end,
-  },
+{
+		"otavioschwanck/arrow.nvim",
+		lazy = false,
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+		opts = {
+			show_icons = true,
+			leader_key = ";",
+			buffer_leader_key = "m",
+			per_buffer_config = {
+				sort_automatically = false
+			}
+		},
+		config = function(_, opts)
+			-- Delete m for bookmark because arrow.nvim use m
+			if vim.fn.maparg('m', 'n') ~= '' then
+				vim.keymap.del('n', 'm')
+			end
+			vim.keymap.set("n", "H", require("arrow.persist").previous)
+			vim.keymap.set("n", "L", require("arrow.persist").next)
+			vim.keymap.set("n", "<C-s>", require("arrow.persist").toggle)
+			vim.keymap.set("n", "<M-h>", require("arrow.commands").commands.prev_buffer_bookmark)
+			vim.keymap.set("n", "<M-l>", require("arrow.commands").commands.next_buffer_bookmark)
+			vim.keymap.set("n", "M", require("arrow.commands").commands.toggle_current_line_for_buffer)
+			require("arrow").setup(opts)
+			-- Custom configuration or commands
+		end,
+	}
 }
